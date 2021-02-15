271fd00cc1d7a8e0aed01fb0979c573657a08a1a
Rafa <joser210507@gmail.com>
2021-02-15 17:00:40 -0400

    pratice-2: created product POJO encapsulation

---
+++ practice-2/app/src/main/java/edu/pucmm/eict/encapsulation/Product.java
@@ -0,0 +1,43 @@
+import java.math.BigDecimal;
+
+public class Product {
+
+  String id;
+  String name;
+  BigDecimal price;
+
+  public Product() {
+
+  }
+
+  public Product(String id, String name, BigDecimal price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public BigDecimal getPrice() {
+    return this.price;
+  }
+
+  public void setPrice (BigDecimal price) {
+    this.price = price;
+  }
+
+}
