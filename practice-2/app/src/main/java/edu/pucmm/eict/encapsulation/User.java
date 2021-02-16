27535eebda2d1daa111e9dee029e3ca4c5bc23c4
Rafa <joser210507@gmail.com>
2021-02-15 21:56:59 -0400

    practice-2: create User encapsulation model

---
+++ practice-2/app/src/main/java/edu/pucmm/eict/encapsulation/User.java
@@ -0,0 +1,42 @@
+
+public class User {
+  
+  String username;
+  String password;
+  String name;
+
+  public User() {
+
+  }
+
+  public User(String username, String password, String name) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+  } 
+
+  public Sring getUserName() {
+    return this.username;
+  }
+
+  public void setUserName(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+}
