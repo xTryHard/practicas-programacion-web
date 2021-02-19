df9f1a2223db5c6fe9a2354444d20fbf89486a75
Rafa <joser210507@gmail.com>
2021-02-19 09:14:23 -0400

    practice-2: add logo

--- practice-2/app/src/main/java/edu/pucmm/eict/encapsulation/Sell.java
+++ practice-2/app/src/main/java/edu/pucmm/eict/encapsulation/Sell.java
@@ -2,7 +2,6 @@
 package edu.pucmm.eict.encapsulation;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
