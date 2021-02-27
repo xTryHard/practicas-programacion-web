586eafe8fed0a71f9144e1db034f6ecd0786e909
Rafa <joser210507@gmail.com>
2021-02-26 20:11:53 -0400

    practice-3: final commit

--- practice-3/app/src/main/java/edu/pucmm/eict/controllers/GeneralControllers.java
+++ practice-3/app/src/main/java/edu/pucmm/eict/controllers/GeneralControllers.java
@@ -17,13 +17,19 @@ import edu.pucmm.eict.services.ShoppingCartServices;
 import edu.pucmm.eict.utils.BaseController;
 import io.javalin.Javalin;
 
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
 public class GeneralControllers extends BaseController {
 
   private String adminMode = "";
   private SellServices sellServices;
+  StrongPasswordEncryptor passwordEncryptor;
+
   public GeneralControllers(Javalin app) {
     super(app);
     sellServices = new SellServices();
+    passwordEncryptor = new StrongPasswordEncryptor();
   }
 
   public void applyRoutes() {
@@ -32,32 +38,50 @@ public class GeneralControllers extends BaseController {
 
       // String admin = ctx.sessionAttribute("admin");
       String user = ctx.sessionAttribute("user");
-      
+      String cookieAuth = ctx.cookie("rememberMe");
+      if (cookieAuth != null) {
+        try {
+          if (passwordEncryptor.checkPassword("autoAuth", cookieAuth)) {
+            ShoppingCartServices.getInstance().setAdminMode(true);
+            ctx.sessionAttribute("admin", "admin");
+            ctx.sessionAttribute("rememberMe", "autoAuth");
+          }
+        } catch (EncryptionOperationNotPossibleException e) {
+          System.out.println(e.toString());
+        }
+      } else {
+        ctx.sessionAttribute("autoAuth", null);
+      }
+
       if (user == null) {
         ctx.sessionAttribute("user", "user");
         ctx.sessionAttribute("shopping-cart", new ShoppingCart());
       }
 
     });
-    
+
     app.before("/shop", ctx -> {
-      String admin = ctx.sessionAttribute("admin"); 
-            //invalidate session if admin leaves admin block
-            if (admin != null) {
-              ctx.sessionAttribute("admin", null);
-              System.out.println("Si");
-              ShoppingCartServices.getInstance().setAdminMode(false);
-            }      
+      String admin = ctx.sessionAttribute("admin");
+      String autoAuthCookie = ctx.sessionAttribute("rememberMe");
+
+      // invalidate session if admin leaves admin block
+      if (admin != null && autoAuthCookie == null) {
+        ctx.sessionAttribute("admin", null);
+        System.out.println("Si");
+        ShoppingCartServices.getInstance().setAdminMode(false);
+      }
     });
 
     app.before("/cart", ctx -> {
-      String admin = ctx.sessionAttribute("admin"); 
-            //invalidate session if admin leaves admin block
-            if (admin != null) {
-              ctx.sessionAttribute("admin", null);
-              System.out.println("Si");
-              ShoppingCartServices.getInstance().setAdminMode(false);
-            }      
+      String admin = ctx.sessionAttribute("admin");
+      String autoAuthCookie = ctx.sessionAttribute("rememberMe");
+
+      // invalidate session if admin leaves admin block
+      if (admin != null && autoAuthCookie == null) {
+        ctx.sessionAttribute("admin", null);
+        System.out.println("Si");
+        ShoppingCartServices.getInstance().setAdminMode(false);
+      }
     });
 
     app.get("/", ctx -> {
@@ -65,9 +89,9 @@ public class GeneralControllers extends BaseController {
     });
 
     app.get("/cart", ctx -> {
-      //Vista del carrito;
-    
-      //Inject products to template
+      // Vista del carrito;
+
+      // Inject products to template
       Map<String, Object> model = new HashMap<String, Object>();
 
       ShoppingCart userShoppingCart = ctx.sessionAttribute("shopping-cart");
@@ -82,9 +106,8 @@ public class GeneralControllers extends BaseController {
       model.put("productAmounts", productAmounts.toArray());
       model.put("productTotalPrice", productTotalPrice.toArray());
       model.put("totalPrice", total);
-    
 
-      //Inject shopping cart to view;
+      // Inject shopping cart to view;
       ctx.render("/templates/cart.html", model);
 
     });
@@ -95,27 +118,32 @@ public class GeneralControllers extends BaseController {
       userShoppingCart.deleteProduct(userShoppingCart.getProductById(productId));
       ctx.sessionAttribute("shopping-cart", userShoppingCart);
 
-      ctx.redirect("/cart");;
+      ctx.redirect("/cart");
+      ;
 
     });
 
-    //Process purchase of the items in the cart
+    // Process purchase of the items in the cart
     app.post("/cart/purchase-cart", ctx -> {
 
       String user = ctx.formParam("clientName");
       ShoppingCart userShoppingCart = ctx.sessionAttribute("shopping-cart");
-      // Sell sell = new Sell(new Date(), user,userShoppingCart.getProducts(), userShoppingCart.getProductAmountList(), userShoppingCart.getProductTotalPrice(), userShoppingCart.getCartTotalPrice());
+      // Sell sell = new Sell(new Date(), user,userShoppingCart.getProducts(),
+      // userShoppingCart.getProductAmountList(),
+      // userShoppingCart.getProductTotalPrice(),
+      // userShoppingCart.getCartTotalPrice());
 
       List<Integer> amountList = userShoppingCart.getProductAmountList();
 
       List<BigDecimal> productTotalPrices = userShoppingCart.getProductTotalPrice();
-      
+
       BigDecimal sellTotalPrice = userShoppingCart.getCartTotalPrice();
 
       List<SoldProduct> soldProducts = new ArrayList<>();
       int i = 0;
       for (Product product : userShoppingCart.getProducts()) {
-        SoldProduct soldProduct = new SoldProduct(product.getId(), product.getName(), product.getPrice(), amountList.get(i), productTotalPrices.get(i));
+        SoldProduct soldProduct = new SoldProduct(product.getId(), product.getName(), product.getPrice(),
+            amountList.get(i), productTotalPrices.get(i));
         soldProducts.add(soldProduct);
         i++;
       }
@@ -123,7 +151,7 @@ public class GeneralControllers extends BaseController {
       Sell sell = new Sell(new Date(), user, soldProducts, sellTotalPrice);
       sellServices.createSell(sell);
       sellServices.createSellProduct(sell);
-      
+
       ShoppingCartServices.getInstance().addSell(sell);
 
       ctx.req.getSession().invalidate();
@@ -133,14 +161,11 @@ public class GeneralControllers extends BaseController {
       model.put("codeHref", "/shop");
       ctx.render("/templates/error.html", model);
 
-
-
-
-      System.out.println("Compra realizada: "+ShoppingCartServices.getInstance().getSells().get(0));
+      System.out.println("Compra realizada: " + ShoppingCartServices.getInstance().getSells().get(0));
     });
 
     app.get("/cart/clear-cart", ctx -> {
-      //Clear cart
+      // Clear cart
       ShoppingCart userShoppingCart = ctx.sessionAttribute("shopping-cart");
       userShoppingCart.clearProductList();
       ctx.redirect("/cart");
@@ -149,34 +174,46 @@ public class GeneralControllers extends BaseController {
     app.post("/login-validate", ctx -> {
       String username = ctx.formParam("username");
       String password = ctx.formParam("password");
+      String rememberMe = ctx.formParam("rememberMe");
 
-      System.out.println(username+" "+password);
+      System.out.println(username + " " + password);
       boolean isAdmin = ShoppingCartServices.getInstance().validateAdmin(username, password);
 
       if (!isAdmin) {
-        ctx.redirect("/login/"+this.adminMode);
+        ctx.redirect("/login/" + this.adminMode);
 
       } else {
+
+        if (rememberMe != null) {
+          ctx.cookie("rememberMe", passwordEncryptor.encryptPassword("autoAuth"), 604800);
+          ctx.sessionAttribute("rememberMe", "autoAuth");
+        }
 
         ctx.sessionAttribute("admin", "admin");
         ShoppingCartServices.getInstance().setAdminMode(true);
 
-        if (this.adminMode.equals("sells")) ctx.redirect("/admin/sells");
-        else if (this.adminMode.equals("admin")) ctx.redirect("/admin");
-        else if (this.adminMode.equals("users")) ctx.redirect("/admin/create-user");
+        if (this.adminMode.equals("sells"))
+          ctx.redirect("/admin/sells");
+        else if (this.adminMode.equals("admin"))
+          ctx.redirect("/admin");
+        else if (this.adminMode.equals("users"))
+          ctx.redirect("/admin/create-user");
       }
     });
 
-    app.before("/login/:mode", ctx-> {
+    app.before("/login/:mode", ctx -> {
       String mode = ctx.pathParam("mode");
       String admin = ctx.sessionAttribute("admin");
 
       if (admin != null) {
-        if (mode.equals("sells")) ctx.redirect("/admin/sells");
-        else if (mode.equals("admin")) ctx.redirect("/admin");
-        else if (mode.equals("users")) ctx.redirect("/admin/create-user");
+        if (mode.equals("sells"))
+          ctx.redirect("/admin/sells");
+        else if (mode.equals("admin"))
+          ctx.redirect("/admin");
+        else if (mode.equals("users"))
+          ctx.redirect("/admin/create-user");
       }
-      
+
     });
 
     app.get("/login/:mode", ctx -> {
@@ -187,13 +224,13 @@ public class GeneralControllers extends BaseController {
       ShoppingCart userShoppingCart = ctx.sessionAttribute("shopping-cart");
 
       model.put("cartAmount", userShoppingCart.getTotalAmount());
-    
+
       ctx.render("/templates/login.html", model);
     });
 
     app.get("/shop", ctx -> {
       List<Product> products = ShoppingCartServices.getInstance().getProducts();
-      //Inject products to template
+      // Inject products to template
       Map<String, Object> model = new HashMap<String, Object>();
 
       ShoppingCart userShoppingCart = ctx.sessionAttribute("shopping-cart");
@@ -217,7 +254,6 @@ public class GeneralControllers extends BaseController {
       ctx.redirect("/shop");
     });
 
-
     app.error(404, ctx -> {
       HashMap<String, Object> model = new HashMap<>();
       model.put("code", "404");
@@ -226,8 +262,34 @@ public class GeneralControllers extends BaseController {
       ctx.render("/templates/error.html", model);
     });
 
-    
-  }
+    app.get("/logout", ctx -> {
 
+    
+      if (ctx.sessionAttribute("rememberMe") != null) {
+        ctx.sessionAttribute("rememberMe", null);
+        ctx.sessionAttribute("admin", null);
+        ctx.removeCookie("rememberMe");
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("code", "205");
+        model.put("codeText", "Has cerrado sesión correctamente");
+        model.put("codeHref", "/shop");
+        ctx.render("/templates/error.html", model);
+
+      } else if(ctx.sessionAttribute("admin") != null) {
+          ctx.sessionAttribute("admin", null);
+          HashMap<String, Object> model = new HashMap<>();
+          model.put("code", "205");
+          model.put("codeText", "Has cerrado sesión correctamente");
+          model.put("codeHref", "/shop");
+          ctx.render("/templates/error.html", model);
+      } else {
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("code", "404");
+        model.put("codeText", "Oops, ¡no has iniciado sesión!");
+        model.put("codeHref", "/shop");
+        ctx.render("/templates/error.html", model);
+      }
+    }); 
+  }
 
 }
